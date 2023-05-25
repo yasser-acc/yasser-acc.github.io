7a3045d7c24cbaff9b8a094b83682c235bb5f315
yasser-acc <yassirmastour1600@gmail.com>
2023-05-25 15:09:40 +0100

    Updated The email folder

--- paradisegym/assets/email/send-email.php
+++ paradisegym/assets/email/send-email.php
@@ -1,20 +1,27 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $submit = $_POST['submit'];
+
+
+    if ($submitValue === 'Submit') {
+    // Set up the email parameters
+    $to = 'yasserdigi@gmail.com';  
+    $headers = "From: $name <$email>";
+    $body = "Subject: $subject\n\n$message";
+
+    // Send the email
+    $success = mail($to, $subject, $body, $headers);
+
+    if ($success) {
+        echo 'Message sent successfully!';
+    } else {
+        echo 'An error occurred while sending the message.';
+    }
 }
-
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-
-$to = "yasserdigi@gmail.com";
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
-
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+}
 ?>
